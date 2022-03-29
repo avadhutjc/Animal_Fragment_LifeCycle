121b829a6db4b86cf7c27367ac4eadd7d935a927
Avadhutjc <Avadhutjc@gmail.com>
2022-03-29 21:41:59 +0530

    completed successfully

--- app/src/main/java/com/ajc/animal/Animal.kt
+++ app/src/main/java/com/ajc/animal/Animal.kt
@@ -2,19 +2,4 @@ package com.ajc.animal
 
 data class Animal(var name: String, var dog1: Int) {
 
-//    private var name: String? = null
-//    private var imageId = 0
-
-//    fun Animal(name: String?, imageId: Int) {
-//        this.name = name
-//        this.imageId = imageId
-//    }
-//
-//    fun getName(): String? {
-//        return name
-//    }
-//
-//    fun getImageId(): Int {
-//        return imageId
-//    }
 }
